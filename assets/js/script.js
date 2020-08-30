1e90b0c172ad849d0f263ad6eb01c656e2db5399
jackattack <jacquelinenicole@me.com>
2020-08-29 21:39:15 -0600

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -154,22 +154,22 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function (event) {
     $(this).addClass("dropover");
-    $(bottom-trash).addClass("bottom-trash-drag");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log("activate", this);
   },
   deactivate: function (event) {
     $(this).removeClass("dropover")
-    $(bottom-trash).removeClass
+    $(".bottom-trash").removeClass
     console.log("deactivate", this);
   },
   over: function (event) {
     $(event.target).addClass("dropover-active");
-    $(bottom-trash).addClass("bottom-trash-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over", event.target);
   },
   out: function (event) {
     $(event.target).removeClass("dropover-active")
-    $(bottom-trash).removeClass("bottom-trash-active")
+    $(".bottom-trash").removeClass("bottom-trash-active")
     console.log("out", event.target);
   },
 
@@ -234,7 +234,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
